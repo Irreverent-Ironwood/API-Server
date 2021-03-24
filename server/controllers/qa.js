c7004e025be16e845feb4c2f1dff9bf0a15ceeb5
Michael Moriarty <mmoriarty316@gmail.com>
2021-03-24 10:41:50 -0700

    Update qa.js

--- server/controllers/qa.js
+++ server/controllers/qa.js
@@ -1,16 +1,16 @@
-const axios = require('axios');
-const path = 'http://localhost:3003';
+// const axios = require('axios');
+// const path = 'http://localhost:3003';
 
-module.exports = {
-  qa: (req, res, next) => {
-    axios(path + req.url)
-    .then(({ data }) => {
-      res.send(data);
-      next();
-    })
-    .catch((err) => {
-      res.status(400);
-      next();
-    })
-  }
-}
+// module.exports = {
+//   qa: (req, res, next) => {
+//     axios(path + req.url)
+//     .then(({ data }) => {
+//       res.send(data);
+//       next();
+//     })
+//     .catch((err) => {
+//       res.status(400);
+//       next();
+//     })
+//   }
+// }
