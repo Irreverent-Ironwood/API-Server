c1db78b469dcff2896c9a032e9d2f582b145e86c
Michael Moriarty <mmoriarty316@gmail.com>
2021-03-24 23:15:07 -0700

    Update routes.js

--- server/routes.js
+++ server/routes.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const controllers = require('./controllers');
 const router = express.Router();
+const { qa } = require('./controllers/qa');
 
-// router.get('/products/*', controllers.qa);
+router.get('/qa/*', qa);
 router.get('/reviews/*', controllers.reviews.getReviews)
 
 
