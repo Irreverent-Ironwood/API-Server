6e11e49e9564e9a340b7aa85f9c402b5554bf2cc
Kanchan Chauhan <kanchan827@gmail.com>
2021-03-25 22:50:38 -0700

    Update reviews.js

--- server/controllers/reviews.js
+++ server/controllers/reviews.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 
 const getReviews = (req, res) => {
 
-  axios.get(`http://ec2-3-92-221-72.compute-1.amazonaws.com:5000${req.url}`)
+  axios.get(`54.86.59.129:5000${req.url}`)
   .then((response) => {
     res.send(response.data)
   })
