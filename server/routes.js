e90c34bb614506035c6a838d30935dcbbaabd9b9
marco garcia <marcogarcia831@gmail.com>
2021-03-27 09:29:03 -0700

    added products route

--- server/routes.js
+++ server/routes.js
@@ -2,9 +2,11 @@ const express = require('express');
 const controllers = require('./controllers');
 const router = express.Router();
 const { qa } = require('./controllers/qa');
+const Products = require('./controllers/Product.js') 
 
 router.get('/qa/*', qa);
 router.get('/reviews/*', controllers.reviews.getReviews)
+router.get('/products/*', controllers.Products.Product)
 
 
 
